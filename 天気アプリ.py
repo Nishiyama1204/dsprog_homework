c724e50779415d216f1dd255fe798ac6bd77e9aa
西山 <s2522108@stu.musashino-u.ac.jp>
2025-12-22 12:01:51 +0900

    地域選択可能に変更

--- 天気アプリ.py
+++ 天気アプリ.py
@@ -2,22 +2,47 @@ import flet as ft
 import requests
 
 def main(page: ft.Page):
-    page.title = "天気予報アプリ（試作）"
+    page.title = "天気予報アプリ"
 
-    weather_text = ft.Text(value="ここに結果が出ます", size=20)
+    weather_text = ft.Text(value="地域を選んでください", size=20)
+    
+    region_dropdown = ft.Dropdown(
+        label="地域を選択",
+        width=300,
+        options=[]
+    )
 
-    def get_weather(e):
-        url = "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json"
-        data = requests.get(url).json()
+    def display_weather(e):
+        if region_dropdown.value is None:
+            weather_text.value = "地域を選択してください！"
+        else:
+            code = region_dropdown.value
+            url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{code}.json"
+            
+            data = requests.get(url).json()
+            forecast = data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+            
+            weather_text.value = f"選択した地域の天気: {forecast}"
+        page.update()
 
-        forecast = data[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+    def load_regions():
+        url = "http://www.jma.go.jp/bosai/common/const/area.json"
+        response = requests.get(url).json()
         
-        weather_text.value = f"東京の天気: {forecast}"
+        offices = response["offices"]
+        
+        for code, info in offices.items():
+            region_dropdown.options.append(
+                ft.dropdown.Option(key=code, text=info["name"])
+            )
         page.update()
 
     page.add(
-        ft.ElevatedButton("東京の天気を取得", on_click=get_weather),
+        region_dropdown,
+        ft.ElevatedButton("天気を表示", on_click=display_weather),
         weather_text
     )
+
+    load_regions()
 
 ft.app(target=main)
